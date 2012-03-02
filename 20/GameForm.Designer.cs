f65021fbf2f74ceda54df6964b7b12cfb722b80a
Daniel Harms <jdharms@gmail.com>
2012-03-02 11:35:18 -0700

    Made room for score/state information.

--- 20/GameForm.Designer.cs
+++ 20/GameForm.Designer.cs
@@ -44,7 +44,7 @@
             // courtBox
             // 
             this.courtBox.Image = global::_20.Properties.Resources.BasketballCourt;
-            this.courtBox.Location = new System.Drawing.Point(23, 22);
+            this.courtBox.Location = new System.Drawing.Point(12, 146);
             this.courtBox.Name = "courtBox";
             this.courtBox.Size = new System.Drawing.Size(1213, 731);
             this.courtBox.TabIndex = 0;
@@ -53,7 +53,7 @@
             // 
             // reboundButton
             // 
-            this.reboundButton.Location = new System.Drawing.Point(23, 759);
+            this.reboundButton.Location = new System.Drawing.Point(12, 883);
             this.reboundButton.Name = "reboundButton";
             this.reboundButton.Size = new System.Drawing.Size(164, 35);
             this.reboundButton.TabIndex = 1;
@@ -62,7 +62,7 @@
             // 
             // turnoverButton
             // 
-            this.turnoverButton.Location = new System.Drawing.Point(193, 759);
+            this.turnoverButton.Location = new System.Drawing.Point(182, 883);
             this.turnoverButton.Name = "turnoverButton";
             this.turnoverButton.Size = new System.Drawing.Size(164, 35);
             this.turnoverButton.TabIndex = 2;
@@ -71,7 +71,7 @@
             // 
             // jumpBallButton
             // 
-            this.jumpBallButton.Location = new System.Drawing.Point(363, 759);
+            this.jumpBallButton.Location = new System.Drawing.Point(352, 883);
             this.jumpBallButton.Name = "jumpBallButton";
             this.jumpBallButton.Size = new System.Drawing.Size(164, 35);
             this.jumpBallButton.TabIndex = 3;
@@ -80,7 +80,7 @@
             // 
             // periodStartButton
             // 
-            this.periodStartButton.Location = new System.Drawing.Point(873, 759);
+            this.periodStartButton.Location = new System.Drawing.Point(862, 883);
             this.periodStartButton.Name = "periodStartButton";
             this.periodStartButton.Size = new System.Drawing.Size(164, 35);
             this.periodStartButton.TabIndex = 5;
@@ -89,7 +89,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(533, 759);
+            this.button6.Location = new System.Drawing.Point(522, 883);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(164, 35);
             this.button6.TabIndex = 6;
@@ -99,14 +99,14 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(1242, 24);
+            this.listBox1.Location = new System.Drawing.Point(1231, 148);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(195, 732);
             this.listBox1.TabIndex = 7;
             // 
             // substitutionButton
             // 
-            this.substitutionButton.Location = new System.Drawing.Point(1043, 759);
+            this.substitutionButton.Location = new System.Drawing.Point(1032, 883);
             this.substitutionButton.Name = "substitutionButton";
             this.substitutionButton.Size = new System.Drawing.Size(164, 35);
             this.substitutionButton.TabIndex = 8;
@@ -115,7 +115,7 @@
             // 
             // timeoutButton
             // 
-            this.timeoutButton.Location = new System.Drawing.Point(703, 759);
+            this.timeoutButton.Location = new System.Drawing.Point(692, 883);
             this.timeoutButton.Name = "timeoutButton";
             this.timeoutButton.Size = new System.Drawing.Size(164, 35);
             this.timeoutButton.TabIndex = 9;
@@ -124,7 +124,7 @@
             // 
             // buttonBox
             // 
-            this.buttonBox.Location = new System.Drawing.Point(12, 749);
+            this.buttonBox.Location = new System.Drawing.Point(1, 873);
             this.buttonBox.Name = "buttonBox";
             this.buttonBox.Size = new System.Drawing.Size(1204, 46);
             this.buttonBox.TabIndex = 10;
@@ -134,7 +134,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1445, 807);
+            this.ClientSize = new System.Drawing.Size(1443, 924);
             this.Controls.Add(this.timeoutButton);
             this.Controls.Add(this.substitutionButton);
             this.Controls.Add(this.listBox1);
