986f1c29294db7f92e847367e00e0684c4746de8
Daniel Harms <jdharms@gmail.com>
2012-03-16 18:13:56 -0700

    Implemented first version of "getGames" and "getGameData" api calls.

---
+++ 20/GameDataResponse.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20
+{
+    class PlayerData
+    {
+        public string playerId;
+        public string teamId;
+        public Dictionary<string, string> name;
+        public int jerseyNumber;
+        public bool isTeamPlayer;
+
+        public override string ToString()
+        {
+            if (isTeamPlayer)
+            {
+                return "Team Player";
+            }
+            return name["lastName"] + ", " + name["firstName"] +
+                " -- " + jerseyNumber;
+        }
+    }
+
+    class TeamData
+    {
+        public string teamName;
+        public string teamId;
+        public List<PlayerData> players;
+
+        public override string ToString()
+        {
+            var ret = "";
+            ret = ret + teamName + "\n";
+            foreach (PlayerData player in players)
+            {
+                ret = ret + "\t" + player + "\n";
+            }
+            return ret;
+        }
+    }
+
+
+    class GameDataResponse
+    {
+        public string time;
+        public string request;
+        public string result;
+
+        public Dictionary<string, TeamData> response;
+
+        public override string ToString()
+        {
+            return response["homeTeam"].ToString() + response["awayTeam"].ToString();
+        }
+    }
+}
---
+++ 20/GamesResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20
+{
+    class GamesResponse
+    {
+        public string time;
+        public string request;
+        public string result;
+        public Dictionary<string, List<Game>> response;
+        string gameId;
+    }
+}
