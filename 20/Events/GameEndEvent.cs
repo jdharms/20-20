b41d79c3ca2d262856b74c0ceb38937c80128a0f
Michael Lemons <lemons.mike@gmail.com>
2012-03-20 19:30:57 -0700

    Intermediate commit for gameend event.
    saving my code.

---
+++ 20/Events/GameEndEvent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace _20.Events
+{
+    class GameEndEvent : Event 
+    {
+        public GameEndEvent(Alpaca pac)
+            : base(pac)
+        {
+            //empty constructor
+        }
+            
+
+        public string serialize()
+        {
+
+            string payload = JsonConvert.SerializeObject(
+            return null;
+        }
+
+        public bool deserialize(string json)
+        {
+
+
+            return false;
+        }
+
+        public void resolve()
+        {
+
+
+
+        }
+
+        public void unresolve()
+        {
+
+
+
+        }
+
+
+    }
+}
