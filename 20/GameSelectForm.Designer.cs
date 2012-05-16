585af151fa0b5bb96747c27c34f2ccf548ac6f74
jnguyenjr <jnguyenjr@gmail.com>
2012-05-15 21:20:07 -0700

    No auto login

--- 20/GameSelectForm.Designer.cs
+++ 20/GameSelectForm.Designer.cs
@@ -132,6 +132,7 @@
             this.button1.Size = new System.Drawing.Size(17, 16);
             this.button1.TabIndex = 9;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // GameSelectForm
